caf7a4e501f92487d5f1a253e6b95c7aabee5e4f
Armando Fox <fox@cs.berkeley.edu>
2017-12-17 13:32:13 -0800

    working minimal version

--- toMobileSheets.pl
+++ toMobileSheets.pl
@@ -17,7 +17,7 @@ sub try_match {
         ($line =~ /^(.*),(\d+),?\s*$/ )) {
         return ($1,$2);
     } else {
-        warn "Couldn't parse line $. in $file:\n$line\n";
+        warn "Ignoring line $. in $file:\n$line\n";
     }
 }
 
@@ -28,9 +28,9 @@ $dir = shift @ARGV;
 die "Usage: $0 <dirname>\nCreates MobileSheets-compatible CSVs in directory 'dirname', which must exist\n" unless $dir && -d $dir;
 
 foreach $file (glob '*.csv') {
-    open(FH,'<',$file) or die "Can't open $file";
+    open($fh,'<',$file) or die "Can't open $file";
     open(OUT,'>',"$dir/$file") or die "Can't write $dir/$file";
-    while (<FH>) {
+    while (<$fh>) {
         chomp;
         next  if /^#/;       # ignore lines beginning with '#'
         next if /^\s*$/;    # ignore blank lines
@@ -38,14 +38,13 @@ foreach $file (glob '*.csv') {
         ($title,$startpage) = try_match($_);
         push(@lines,"$startpage,$title");
     }
-    
+    close $fh;
     print OUT "title,pages\n";
     foreach $_ (sort @lines) {
         if (/^(\d+),(.*)$/) {
             printf OUT ("%s,%d-%d\n", $2, $1, $1+$offset);
         } 
     }
-    printf STDERR "$file -->  $dir/$file\n";
 }
 
 
